edfc826fafd57d60e5cf3ac82c15e27397e9503e
kcamp23 <89957266+kcamp23@users.noreply.github.com>
2022-01-10 13:24:11 -0500

    notes- daily commit

---
+++ src/com/careerdevs/intro/FIZZBUZZ.java
@@ -0,0 +1,16 @@
+package com.careerdevs.intro;
+
+import java.util.Scanner;
+
+public class FIZZBUZZ {
+
+        public static void main(String [] args ){
+            Scanner scaner = new Scanner(System.in);
+            System.out.print("Number:");
+            int number = scanner.nextInt();
+
+
+        }
+
+
+}
