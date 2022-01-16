198d5c9d0b6b24975f73b3caeab3bf9a40be4a5d
Kat Camp <kcamp552369@gmail.com>
2022-01-16 17:01:24 -0500

    Fizz Buzz

--- src/com/careerdevs/intro/FIZZBUZZ.java
+++ src/com/careerdevs/intro/FIZZBUZZ.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class FIZZBUZZ {
 
         public static void main(String [] args ){
-            Scanner scaner = new Scanner(System.in);
+            Scanner scanner = new Scanner(System.in);
             System.out.print("Number:");
             int number = scanner.nextInt();
 
