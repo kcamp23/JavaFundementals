6bf59b51f63cd90a78a623e23f9d13bd9aa1aacb
Kat Camp <kcamp552369@gmail.com>
2022-01-17 17:04:25 -0500

    Mortgage Calculator

--- src/com/careerdevs/intro/MortgageCalculator.java
+++ src/com/careerdevs/intro/MortgageCalculator.java
@@ -10,16 +10,15 @@ public class MortgageCalculator {
         System.out.print("Principal:");
         int principal = scanner.nextInt();
 
-
         System.out.print("Annual Interest Rate:");
         float annualInterest = scanner.nextFloat();
-        float monthlyInterest = annualInterest/ PERCENT / MONTHS
+        float monthlyInterest = annualInterest/ PERCENT / MONTHS;
 
         System.out.print("Period(Years):");
         byte years = scanner.nextByte();
         int payments = years * MONTHS;
 
-        double mortgage = principal * (monthlyInterest * Math.pow(1 + monthlyInterest,payments))
+        double mortgage = principal * (monthlyInterest * Math.pow(1 + monthlyInterest,payments));
 
 
     }
